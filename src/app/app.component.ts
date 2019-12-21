8e1cf73a253bf53e3fae1c8360c56440ca5d6a62
Chris Morgan <bluemorgan2323@gmail.com>
2019-12-20 20:51:45 -0600

    Still working on this dang slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,16 +26,127 @@ export class AppComponent {
       url: '../assets/MomPhotos/MomStaceyBeachWedding.jpg'
     },
     {
-      url: '../assets/MomPhotos/MomStaceyChrisGapTooth.jpg'
+      url: '../assets/MomPhotos/ChrisVintageEaster.jpg'
     },
     {
-      url: '../assets/MomPhotos/JuniorAuxillary.jpg'
+      url: '../assets/MomPhotos/CannonCampbellSilly.jpg'
     },
     {
-      url: '../assets/MomPhotos/JuniorAuxillary.jpg'
+      url: '../assets/MomPhotos/CannonWrestlingVictory.jpg'
     },
     {
-      url: '../assets/MomPhotos/JuniorAuxillary.jpg'
+      url: '../assets/MomPhotos/ChrisGIJoeHalloween.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/ElvesEatingSugar.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/CannonBirthdaySetup.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/CannonCampbellFlags.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/BrownsCampbell.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MatthewGrad.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/CannonHalloweenCostume.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/ChrisCannonCampbellGrinch.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/ChrisMomMudIsland.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/FamilyDisneyCruisePhoto.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/FriendsMomStacey.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/FunnyCannonCard.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/HalloweenCampbell.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomCampbellCrying.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomCannonColorado.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomChrisChristmasVintage.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomDixie.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomHalloweenNmB4Christmas.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomStacey.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomStaceyCambpell.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomChrisGlobetrotters.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomNurse.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomSmilingGift.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomLukeSkyWalker.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomStaceyHalloween.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomLaughingDickBday.jpg'
+    }
+    ,
+    {
+      url: '../assets/MomPhotos/MomChrisOldPhoto.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MomStaceyWeddingJump.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/MothersDayWithChels.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/PittmansCampbell1.jpg'
+    },
+    {
+      url: '../assets/MomPhotos/StaceyNote.jpg'
     }
   ]
 }
